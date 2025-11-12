373a3a9573c8eba740e4bc990f4568974f397150
archerc68 <archerc68@gmail.com>
2025-11-12 12:32:36 +0000

    Fixed Linear FDE solver to move out of beta

--- Linear FDE solver.py
+++ Linear FDE solver.py
@@ -15,32 +15,33 @@ import matplotlib.pyplot as plt
 "d_i:           d_i = y^(i)(0) -- Boundary conditions"
 "g(x):          RHS perturbing function"
 
-L = 2 * np.pi
+L = 2
 
 # FDE Params
 m = 25
-alpha = 3.1
-beta_k = np.array([0.3, 2.1])
-k = len(beta_k)
+alpha = 2
+beta_k = np.array([1.5])
+k = len(beta_k) - 1
 assert alpha > np.max(beta_k)
 
 
 def g(x):
-    return np.sin(x)
+    return 1 + x
 
 
-a_k = np.array([1, -1, 0.1, 1])
+a_k = np.array([-1, -1, 1])
+assert len(a_k) == k + 3
 
 n = int(np.floor(alpha))
 
-d_i = np.array([1, -1, 0])
+d_i = np.array([1, 1])
 assert len(d_i) == n
 
 
 # region auxilliary parameters
 
 # Auxilliary parameters
-x = np.linspace(0, L, 250)
+x = np.linspace(0, L, 100)
 t = 2 * x / L - 1
 
 
@@ -51,31 +52,48 @@ t = 2 * x / L - 1
 
 
 def D(N, nu):
-    LB = int(np.ceil(nu))
+    if nu == 0:
+        return np.eye(N+1)
+    else:
+        LB = int(np.ceil(nu))
 
-    i = np.arange(N + 1)[:, None]
-    j = np.arange(N + 1)[None, :]
-    D_matrix = np.zeros((N + 1, N + 1))
+        i = np.arange(N + 1)[:, None]
+        j = np.arange(N + 1)[None, :]
+        D_matrix = np.zeros((N + 1, N + 1))
 
-    eps_j = np.ones_like(j)
-    eps_j[:, 0] = 2
+        eps_j = np.ones_like(j)
+        eps_j[:, 0] = 2
 
-    for k in range(LB, N + 1):
-        sign = np.where((i - k) % 2 == 0, 1, -1)
-        num = sign * 2 * i * factorial(i + k - 1) * gamma(k - nu + 0.5)
-        den = (
-            1
-            / eps_j
-            * 1
-            / (L**nu)
-            * rgamma(k + 0.5)
-            * rgamma(i - k + 1)
-            * rgamma(k - j - nu + 1)
-            * rgamma(k + j - nu + 1)
-        )
-        term = np.where(k <= i, num * den, 0)
-        D_matrix += np.nan_to_num(term, 0)
-    return np.nan_to_num(D_matrix, 0)
+        for k in range(LB, N + 1):
+            sign = np.where((i - k) % 2 == 0, 1, -1)
+            num = sign * 2 * i * factorial(i + k - 1) * gamma(k - nu + 0.5)
+            den_inv = (
+                1
+                / eps_j
+                * 1
+                / (L**nu)
+                * rgamma(k + 0.5)
+                * rgamma(i - k + 1)
+                * rgamma(k - j - nu + 1)
+                * rgamma(k + j - nu + 1)
+            )
+            den_inv = np.nan_to_num(den_inv, 0)
+            term = np.where(k <= i, num * den_inv, 0)
+            D_matrix += term
+        return D_matrix
+    
+def D_1(N):
+    D_matrix_T = np.zeros((N + 1, N + 1))
+    k = np.arange(1, N + 1, 2)
+
+    for i in k:
+        D_matrix_T += np.diagflat(np.arange(i, N + 1), i)
+    D_matrix = D_matrix_T.T
+    D_matrix[:, 0] /= 2
+
+    D_matrix *= 4/L
+    return D_matrix
+
 
 # endregion
 
@@ -89,7 +107,7 @@ def D(N, nu):
 phi = chebvander(t, m).T
 phi_0 = phi[:, 0]
 
-
+# G_T
 def G_guess_var(G_0_T):
     return G_0_T @ phi - g(x)
 
@@ -99,6 +117,12 @@ result = least_squares(G_guess_var, G_0_T)
 
 G_T = result.x
 
+# plt.figure()
+# plt.plot(x, g(x))
+# plt.plot(x, G_T @ phi)
+# plt.show()
+
+
 # D'
 D_alpha = D(N=m, nu=alpha)
 D_beta_sum = np.zeros((m + 1, m + 1))
@@ -106,21 +130,29 @@ for i in range(k):
     D_beta_sum += a_k[i] * D(N=m, nu=beta_k[i])
 D_prime = D_alpha - D_beta_sum - a_k[k + 1] * np.eye(m + 1)
 
-
+# Operating matrix
 Operator = np.empty((m + 1, m + 1))
-Operator[:, : m - n - 1] = D_prime[:, : m - n - 1]
+Operator[:, :] = D_prime[:, :]
 
-for i in range(n):
-    Operator[:, m - n + i] = D(N=m, nu=i) @ phi_0
+# First derivative operator
+D1 = D_1(N=m)
 
+# Boundary conditions
+Operator[:, m - n + 1] = phi_0
+if n >= 1:
+    for i in range(1, n):
+        Operator[:, m - n + 1 + i] = D1 @ Operator[:, m - n + i]
+
+# Inverse of operator matrix
 Operator_inv = np.linalg.inv(Operator)
 
+# Column vector
 column_vec = np.empty(m + 1)
 column_vec[: m - n + 1] = G_T[: m - n + 1]
 column_vec[m - n + 1 :] = d_i
 
 
-y = (column_vec @ Operator_inv) @ phi
+y = column_vec @ Operator_inv @ phi
 
 
 # endregion
@@ -128,4 +160,5 @@ y = (column_vec @ Operator_inv) @ phi
 if __name__ == "__main__":
     plt.figure()
     plt.plot(x, y)
+    plt.plot(x, x+1)
     plt.show()
--- Testy.py
+++ Testy.py
@@ -1,119 +1,18 @@
 import numpy as np
-from scipy.special import gamma, factorial
-import time
-from numba import njit
 
+L=2
 
-# --- Original triple-loop version ---
-def D_loop(N, nu, L=1.0):
-    LB = int(np.ceil(nu))
+def D_1(N):
+    D_matrix_T = np.zeros((N + 1, N + 1))
+    k = np.arange(1, N + 1, 2)
 
-    def eps(j):
-        return 2 if j == 0 else 1
+    for i in k:
+        D_matrix_T += np.diagflat(np.arange(i, N + 1), i)
+    D_matrix = D_matrix_T.T
+    D_matrix[:, 0] /= 2
 
-    D_matrix = np.zeros((N + 1, N + 1))
-    for i in range(LB, N + 1):
-        for k in range(LB, i + 1):
-            num = (-1) ** (i - k) * 2 * i * factorial(i + k - 1) * gamma(k - nu + 0.5)
-            for j in range(N + 1):
-                den = (
-                    eps(j)
-                    * (L**nu)
-                    * gamma(k + 0.5)
-                    * factorial(i - k)
-                    * gamma(k - j - nu + 1)
-                    * gamma(k + j - nu + 1)
-                )
-                D_matrix[i, j] += num / den
+    D_matrix *= 4/L
     return D_matrix
 
 
-# --- First vectorized version (with mask) ---
-def D_vec(N, nu, L=1.0):
-    LB = int(np.ceil(nu))
-
-    j = np.arange(N + 1)
-    eps = np.ones_like(j, dtype=float)
-    eps[0] = 2.0
-
-    i = np.arange(LB, N + 1)[:, None]
-    k = np.arange(LB, N + 1)[None, :]
-
-    mask = k <= i
-
-    num = (
-        np.where((i - k) % 2 == 0, 1, -1)
-        * (2 * i)
-        * factorial(i + k - 1)
-        * gamma(k - nu + 0.5)
-    )
-
-    denom_base = gamma(k + 0.5) * (L**nu)
-
-    fact_ik = factorial(i - k, exact=False)
-    fact_ik[~mask] = np.inf
-
-    j_arr = j[None, None, :]
-    den = (
-        eps[j_arr]
-        * denom_base[..., None]
-        * fact_ik[..., None]
-        * gamma(k[..., None] - j_arr - nu + 1)
-        * gamma(k[..., None] + j_arr - nu + 1)
-    )
-
-    contrib = num[..., None] / den
-    D_matrix = np.zeros((N + 1, N + 1))
-    D_matrix[LB:, :] = np.sum(contrib, axis=1)
-
-    return D_matrix
-
-
-# --- Optimized meshgrid version ---
-def D_matrix(N, nu, L=1.0):
-    i = np.arange(N + 1)[:, None]   # column vector
-    j = np.arange(N + 1)[None, :]   # row vector
-    eps_j = np.where(j == 0, 2, 1)
-
-    k_min = int(np.ceil(nu))
-    D = np.zeros((N+1, N+1))
-
-    for k in range(k_min, N+1):
-        # broadcast over i,j
-        sign = np.where(((i - k) % 2) == 0, 1.0, -1.0)
-        num = sign * (2 * i) * factorial(i + k - 1) * gamma(k - nu + 0.5)
-
-        den = (
-            eps_j * (L**nu) * gamma(k + 0.5) *
-            gamma(i - k + 1) * gamma(k - nu - j + 1) * gamma(k + j - nu + 1)
-        )
-
-        term = np.where(k <= i, num / den, 0.0)
-        D += term   # accumulate over k
-
-    return D
-
-
-# --- Benchmark runner ---
-def benchmark(funcs, Ns, nu=1.5):
-    results = {}
-    for f in funcs:
-        times = []
-        for N in Ns:
-            start = time.perf_counter()
-            f(N, nu)
-            elapsed = time.perf_counter() - start
-            times.append(elapsed)
-        results[f.__name__] = times
-    return results
-
-
-# Run benchmarks
-Ns = [10, 20, 40, 80]
-funcs = [D_loop, D_vec, D_matrix]
-results = benchmark(funcs, Ns)
-
-# Print results
-print("N values:", Ns)
-for name, times in results.items():
-    print(f"{name:10s}:", ["{:.4f}s".format(t) for t in times])
+D_1(6)
